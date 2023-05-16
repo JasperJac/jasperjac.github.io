b7751bf7a9167ef95f968173341ff2f7f2dffd28
JasperJac <132090977+JasperJac@users.noreply.github.com>
2023-05-16 16:05:57 -0700

    .fix: update resume

--- src/components/layouts/HeaderButton.js
+++ src/components/layouts/HeaderButton.js
@@ -5,7 +5,7 @@ const HeaderButton = () => {
     return (
         <>
             <div className="mx-auto">
-                <button className="see" onClick={ ()=> window.open(`${process.env.PUBLIC_URL}/Jasper_Jack.pdf`, "_blank")}>My resume<i className="fas fa-eye"></i></button>
+                <button className="see" onClick={ ()=> window.open(`${process.env.PUBLIC_URL}/JasperJack.pdf`, "_blank")}>My resume<i className="fas fa-eye"></i></button>
             </div>
         </>
     )
